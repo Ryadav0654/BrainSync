dba02d3a3a16c07723c495987934aad18ebe7cb7
Ravindra Yadav <yadavra2005@gmail.com>
2025-04-05 16:40:57 +0530

    fix type issue

--- app/api/content/[id]/route.ts
+++ app/api/content/[id]/route.ts
@@ -2,34 +2,34 @@ import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "@/libs/auth";
 import { getServerSession } from "next-auth/next";
 import { prisma } from "@/db";
-export async function DELETE(req: NextRequest,  context: { params: { id: string } }) {
-    const params  = await context.params;
-    const { id } = params;
-    const session = await getServerSession(authOptions);
-  
-    if (!session) {
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
-  
-    try {
-      const deletedContent = await prisma.content.delete({
-        where: {
-          id: id,
-          userId: session.user.id
-        },
-      });
-  
-      if (!deletedContent) {
-        return NextResponse.json(
-          { error: "Something went wrong" },
-          { status: 500 }
-        );
-      }
-      return NextResponse.json({ message: "Content deleted successfully" });
-    } catch (error) {
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const id = (await params).id
+
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  try {
+    const deletedContent = await prisma.content.delete({
+      where: {
+        id: id,
+        userId: session.user.id,
+      },
+    });
+
+    if (!deletedContent) {
       return NextResponse.json(
-        { error: error },
+        { error: "Something went wrong" },
         { status: 500 }
       );
     }
+    return NextResponse.json({ message: "Content deleted successfully" });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
   }
+}
